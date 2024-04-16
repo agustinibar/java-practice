630c4f2c99fbc9571d389ee26244b7a5360330b4
agustinibar <agustinibarperrotta@gmail.com>
2024-04-16 02:06:28 -0300

    variables y datos

--- src/App.java
+++ src/App.java
@@ -1,5 +1,34 @@
+import java.util.Scanner;
+
 public class App {
     public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
+        // comentario de una sola linea
+        System.out.println("This is my firs Hello World in Java!");
+        
+        // variables: siempre debemos especificar el tipo de dato
+
+        int edad = 27;
+        String nombre = "Agustin Ibar";
+        Boolean verdadero = true;
+
+        System.out.println(nombre);
+
+        //concatenacion con +
+
+        System.out.println("I'm" + nombre + " and I'm" + edad + "years old.");
+
+        //concatenacion con printf
+
+        System.out.printf("My name is %s and I'm %d  years old", nombre, edad);
+
+        //entrada de datos
+
+        Scanner sc = new Scanner(System.in); //con el .in tomamos lo que entra por sistema
+
+        System.out.println("Como te llamas?: ");
+
+        String name = sc.nextLine();
+
+        System.out.println("The user's name is: " + name);
     }
 }
